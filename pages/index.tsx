1d8df8dc4f0571b8c679f591c8dbad1badb35b6c
Koder <koder1554@gmail.com>
2021-05-12 19:38:26 +0300

    add style files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const HomePage = () => {
-    return <div></div>;
+    return <>
+        <Hero></Hero>
+        <FeaturedPosts></FeaturedPosts>
+    </>;
 };
 
 export default HomePage;
